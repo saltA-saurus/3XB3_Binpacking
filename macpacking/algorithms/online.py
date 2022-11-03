e674d2ee4264f61a119c57994093590f3ce566ab
Nicholas Fabugais-Inaba <fabugain@mcmaster.ca>
2022-11-03 17:11:29 -0400

    implemented firstfit algorithm

--- macpacking/algorithms/online.py
+++ macpacking/algorithms/online.py
@@ -28,54 +28,29 @@ class Terrible(Online):
                 solution.append([w])
         return solution
 
-# class FirstFit(Online):
+class FirstFit(Online):
 
-#     def _process(self, capacity: int, stream: WeightStream) -> Solution:
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
         
-#         bin_index = 0
-#         solution = [[]]
-#         bin_remaining = [0]*len(stream)
-#         for w in stream:
-#             j = 0
-#             while j < bin_index:
-#                 if bin_remaining[j] >= w:
-#                     solution[bin_index] = solution[bin_index] - w 
-#                     break
-#                 j += 1
+        bin_index = 0
+        solution = []
+        bin_remaining = []
+        for w in stream:
+            j = 0
+            while j < bin_index:
+                if bin_remaining[j] >= w:
+                    solution[j].append(w)
+                    bin_remaining[j] -= w 
+                    break
+                j += 1
 
-#             if j == bin_index:
+            if j == bin_index:
+                solution.append([w])
+                bin_remaining.append(capacity)
+                bin_remaining[bin_index] = capacity - w
+                bin_index += 1
 
-
-#             if remaining >= w:
-#                 solution[bin_index].append(w)
-#                 remaining = remaining - w
-#             else:
-#                 bin_index += 1
-#                 solution.append([w])
-#                 remaining = capacity - w
-#         return solution
-    
-    # # Create an array to store remaining space in bins
-    # # there can be at most n bins
-    # bin_rem = [0]*n
-     
-    # # Place items one by one
-    # for i in range(n):
-       
-    #     # Find the first bin that can accommodate
-    #     # weight[i]
-    #     j = 0
-    #     while( j < res):
-    #         if (bin_rem[j] >= weight[i]):
-    #             bin_rem[j] = bin_rem[j] - weight[i]
-    #             break
-    #         j+=1
-             
-    #     # If no bin could accommodate weight[i]
-    #     if (j == res):
-    #         bin_rem[res] = c - weight[i]
-    #         res= res+1
-    # return res
+        return solution
 
 class BestFit(Online):
 

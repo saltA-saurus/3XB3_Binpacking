bb4f993ab6a7e7f3b9e2a5cd158b7b8089f0aa33
Jung Woo Lee <lee331@mcmaster.ca>
2022-11-03 17:38:26 -0400

    added WorstFit

--- macpacking/algorithms/online.py
+++ macpacking/algorithms/online.py
@@ -67,7 +67,37 @@ class WorstFit(Online):
     def _process(self, capacity: int, stream: WeightStream) -> Solution:
         
         solution = []
+
+        # Initialize result (Count of bins)
+        bin_index = 0
+        bin_remaining = []
+        
+        # Place items one by one
         for w in stream:
-            if capacity >= w:
-                solution.append(w)
+            print(w)
+            # Find the best bin that can accommodate
+            # w
+    
+            # Initialize maximum space left and index
+            # of worst bin
+            mx,wi = -1,0
+    
+            for j in range(bin_index):
+                if (bin_remaining[j] >= w and bin_remaining[j] - w > mx):
+                    wi = j
+                    mx = bin_remaining[j] - w
+                
+    
+            # If no bin could accommodate w[i],
+            # create a new bin
+            if (mx == -1):
+                solution.append([w])
+                bin_remaining.append(capacity)
+                bin_remaining[bin_index] = capacity - w
+                bin_index += 1
+            
+            else: # Assign the item to best bin
+                solution[wi].append(w)
+                bin_remaining[wi] -= w
+        
         return solution

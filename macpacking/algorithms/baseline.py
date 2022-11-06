d3352e1d99fa51c6ec5bec5d81c942ac17ee8b7b
Nicholas Fabugais-Inaba <fabugain@mcmaster.ca>
2022-11-06 14:08:25 -0500

    added baseline

--- macpacking/algorithms/baseline.py
+++ macpacking/algorithms/baseline.py
@@ -7,3 +7,8 @@ class BenMaier(Offline):
 
     def _process(self, capacity: int, weights: list[int]) -> Solution:
         return bp.to_constant_volume(weights, capacity)
+
+class BenMaierMNP(Offline):
+
+    def _process(self, bin_num: int, weights: list[int]) -> Solution:
+        return bp.to_constant_bin_number(weights, bin_num)

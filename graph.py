4aa2b3d329cbc4182870579f0c01ff67652fff55
Nicholas Fabugais-Inaba <fabugain@mcmaster.ca>
2022-11-06 21:35:38 -0500

    even more flake8

--- graph.py
+++ graph.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import json
 
+
 class GetData:
 
-    def load_json_data(filename): # filename = 'out/plot_data.json'
+    def load_json_data(filename):  # filename = 'out/plot_data.json'
         file = open(filename)
         plot_data = json.load(file)
         file.close()
@@ -13,20 +14,23 @@ class GetData:
         algorithms = list(plot_data.keys())
         datasets = list(plot_data[algorithms[0]].keys())
 
-        online_data = [[] for i in range(len(datasets))]
-        offline_data = [[] for i in range(len(datasets))]
+        online_data = [[] for _ in range(len(datasets))]
+        offline_data = [[] for _ in range(len(datasets))]
 
-        # algorithms: NextFitOnline, FirstFitOnline, BestFitOnline, WorstFitOnline, NextFitOffline, FirstFitOffline, BestFitOffline, WorstFitOffline
+        # algorithms: NextFitOnline, FirstFitOnline,
+        # BestFitOnline, WorstFitOnline, NextFitOffline,
+        # FirstFitOffline, BestFitOffline, WorstFitOffline
         # datasets: N4C2W2_A.BPP.txt N4C2W2_B.BPP.txt
-        for i in range(len(algorithms)): # 0 - 7
+        for i in range(len(algorithms)):  # 0 - 7
             isOnline = True
             if 'Offline' in algorithms[i]:
                 isOnline = False
-            for j in range(len(datasets)): # A - T
+            for j in range(len(datasets)):  # A - T
+                p_d = plot_data[algorithms[i]][datasets[j]][kpi]
                 if isOnline:
-                    online_data[j].append(float(plot_data[algorithms[i]][datasets[j]][kpi]))
+                    online_data[j].append(float(p_d))
                 elif not isOnline:
-                    offline_data[j].append(float(plot_data[algorithms[i]][datasets[j]][kpi]))
+                    offline_data[j].append(float(p_d))
 
         return (online_data, offline_data, algorithms, datasets)
 
@@ -37,9 +41,8 @@ class GetData:
         algorithms = plot_data[2]
         datasets = plot_data[3]
 
-        average_online_data = [0 for i in range(len(online_data[0]))]
-        average_offline_data = [0 for i in range(len(offline_data[0]))]
-
+        average_online_data = [0 for _ in range(len(online_data[0]))]
+        average_offline_data = [0 for _ in range(len(offline_data[0]))]
 
         for i in range(len(datasets)):
             for j in range(len(online_data[0])):
@@ -52,7 +55,9 @@ class GetData:
         for i in range(len(average_offline_data)):
             average_offline_data[i] /= len(datasets)
 
-        avg_plot_data = (average_online_data, average_offline_data, algorithms, datasets)
+        avg_plot_data = (average_online_data,
+                         average_offline_data,
+                         algorithms, datasets)
 
         return avg_plot_data
 
@@ -64,11 +69,10 @@ class PlotGraph:
         online_data = plot_data[0]
         offline_data = plot_data[1]
         algorithms = plot_data[2]
-        datasets = plot_data[3]
 
         n1 = len(online_data)
         n2 = len(offline_data)
-        r1 = [] 
+        r1 = []
         for i in range(max(n1, n2)):
             r1.append(i)
         width = 0.25
@@ -99,8 +103,8 @@ class PlotGraph:
         plt.bar(r1, online_data, color='orangered',
                 width=width, edgecolor='black',
                 label='Online')
-        plt.bar(r2, offline_data, color = 'deepskyblue',
-                width=width, edgecolor = 'black',
+        plt.bar(r2, offline_data, color='deepskyblue',
+                width=width, edgecolor='black',
                 label='Offline')
 
         plt.xlabel(x_axis_label)
